a83e4dac0aae9fdd403297ba6eaf8a8a925cfd8c
Oli Dacombe <odacombe@blockchain.com>
2022-07-21 23:14:58 +0100

    sketch

---
+++ src/lib.rs
@@ -0,0 +1,49 @@
+use consul::kv::{KVPair, KV};
+use consul::{Client, Config, QueryOptions};
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum WatcherError {
+    #[error(transparent)]
+    Consul(#[from] consul::errors::Error),
+}
+
+struct Watcher {
+    client: Client,
+    path: String,
+}
+
+impl Watcher {
+    pub fn new(path: String) -> Result<Self, WatcherError> {
+        Ok(Self {
+            client: Client::new(Config::new_from_env()?),
+            path,
+        })
+    }
+    pub fn run<F: Fn(Option<KVPair>)>(&self, callback: F) {
+        let mut opts = QueryOptions {
+            datacenter: None,
+            wait_index: None,
+            wait_time: None,
+        };
+
+        loop {
+            match self.client.get(&self.path, Some(&opts)) {
+                Ok((kv, meta)) => {
+                    opts.wait_index = meta.last_index;
+                    callback(kv);
+                }
+                Err(e) => tracing::error!("{:?}", e),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
